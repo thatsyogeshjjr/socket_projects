0bc84dd885da2d05efc658a57f8cacc2fb02b105
Yogesh Jajoria <yogeshjajoria2019@gmail.com>
2024-03-31 23:11:18 +0530

    server now handles post requests.

--- web_server/server_methods_handle.py
+++ web_server/server_methods_handle.py
@@ -2,6 +2,12 @@ import socket
 # to implement threading
 
 
+'''
+TODO:
+* handle POST methods with help of a form
+
+'''
+
 SERVER = 'localhost'
 PORT = 5998
 
@@ -11,12 +17,31 @@ server.bind((SERVER, PORT))
 server.listen()
 print(f'[+]\tserver is active on {SERVER}@{PORT}')
 
+
+def render_page():
+    file = req_data[1][1:]
+    if '?' in file:
+        file, params = file.split('?')[0], file.split('?')[
+            1].split('&')
+        print(f'[+]\tParameters:{params}')
+
+    # valid if we plan to server only html pages
+    if file[-5:] != '.html':
+        file = file+'.html'
+
+    file = open(file).read()
+    client.send(b'HTTP/1.1 200 OK\r\n\r\n')
+    for i in range(len(file)):
+        client.send(file[i].encode())
+
+    client.send(b'\r\n\r\n')
+
+
 while True:
     client, addr = server.accept()
     print(f'[+]\t{addr} sent a request')
 
     req_data = client.recv(1024).decode()
-    print(req_data)
     if not req_data:
         print('[-]\tMethod not allowed. Closing connection')
         client.sendall(b'HTTP/1.1 405 Method Not Allowed\r\n\r\n')
@@ -26,22 +51,15 @@ while True:
     match req_data[0]:
         case 'GET':
             print('[+]\tRequest type: GET')
-            file = req_data[1][1:]
-            if '?' in file:
-                file, params = file.split('?')[0], file.split('?')[
-                    1].split('&')
-                print(f'[+]\tParameters:{params}')
-            file = open(file).read()
-            client.send(b'HTTP/1.1 200 OK\r\n\r\n')
-            for i in range(len(file)):
-                client.send(file[i].encode())
-            client.send(b'\r\n\r\n')
+            render_page()
+            client.close()
 
         case 'POST':
             print('[+]\tRequest type: POST')
-            print()
-            pass
+            print(f'[!]\tRequest Data: {req_data[-1].split("&")}')
+            render_page()
+            client.close()
 
-    client.close()
-    server.close()
-    break
+    # client.close()
+    # server.close()
+    # break
