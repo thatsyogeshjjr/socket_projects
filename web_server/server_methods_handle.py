6e90cec30864eea964b0372be89dd820bbb18d0a
Yogesh Jajoria <yogeshjajoria2019@gmail.com>
2024-03-31 23:50:18 +0530

    variable data can be added in html files

--- web_server/server_methods_handle.py
+++ web_server/server_methods_handle.py
@@ -18,18 +18,48 @@ server.listen()
 print(f'[+]\tserver is active on {SERVER}@{PORT}')
 
 
-def render_page():
+def render_page(req_type=None, http_param_data=None):
+    '''
+    POST request sends parameters in http request.
+    We use http_param_data to handle such data
+
+    Condition for parameters below checks if parameters exist
+    inside the URL (will be the case for GET requests)
+    '''
     file = req_data[1][1:]
-    if '?' in file:
-        file, params = file.split('?')[0], file.split('?')[
-            1].split('&')
-        print(f'[+]\tParameters:{params}')
+    indata = None
+    if req_type == 'POST':
+        print(http_param_data)
+        indata = {}
+        for data_unit in params:
+            data, val = data_unit.split('=')
+            indata[data] = val
+
+        print(f'[+]\t Altered params: {indata}')
+
+    else:
+        if '?' in file:
+            file, params = file.split('?')[0], file.split('?')[
+                1].split('&')
+            print(f'[+]\tParameters:{params}')
+            indata = {}
+            for data_unit in params:
+                data, val = data_unit.split('=')
+                indata[data] = val
+
+            print(f'[+]\t Altered params: {indata}')
 
     # valid if we plan to server only html pages
     if file[-5:] != '.html':
         file = file+'.html'
 
     file = open(file).read()
+
+    if indata:
+        for key, val in indata.items():
+            file = file.replace('{{' + key + '}}', val)
+            print('[!] REPLACED')
+
     client.send(b'HTTP/1.1 200 OK\r\n\r\n')
     for i in range(len(file)):
         client.send(file[i].encode())
@@ -55,9 +85,10 @@ while True:
             client.close()
 
         case 'POST':
+            http_param_data = req_data[-1].split("&")
             print('[+]\tRequest type: POST')
-            print(f'[!]\tRequest Data: {req_data[-1].split("&")}')
-            render_page()
+            print(f'[!]\tRequest Data: {http_param_data}')
+            render_page('POST', http_param_data)
             client.close()
 
     # client.close()
