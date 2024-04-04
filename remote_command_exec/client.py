60400c1242ddf45dd29e79bbf6f4ae698ac4ffc8
Yogesh Jajoria <yogeshjajoria2019@gmail.com>
2024-04-04 23:01:33 +0530

    remote command exec test complete on local network successfully. Problem faced with installing app via scoop.

--- remote_command_exec/client.py
+++ remote_command_exec/client.py
@@ -9,7 +9,16 @@ PORT = int(sys.argv[2])
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((SERVER, PORT))
 
-buff_size = int(s.recv(1024).decode())
-cmd = s.recv(buff_size).decode()
-print(f'To execute: {cmd}')
-# subprocess.run(cmd.split())
+while True:
+    buff_size = int(s.recv(1024).decode())
+    cmd = s.recv(buff_size).decode()
+    if cmd == '000':
+        break
+    print(f'To execute: {cmd.split()}')
+
+    output = subprocess.check_output(cmd.split(), shell=True).decode()
+    # print(type(output))
+    output_buffer = str(len(output))
+
+    s.send(output_buffer.encode())
+    s.send(output.encode())
--- remote_command_exec/server.py
+++ remote_command_exec/server.py
@@ -1,6 +1,7 @@
 import socket
 SERVER = '127.0.0.1'
 PORT = 4444
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((SERVER, PORT))
 s.listen()
@@ -11,3 +12,10 @@ while True:
     cmd = input('Command> ')
     client.send(str(len(cmd)).encode())
     client.send(cmd.encode())
+
+    output_buf = client.recv(1024)
+    print(f'OUTPUT BUFFER:::  {output_buf}')
+    print(f'DECODED::: {output_buf.decode()}')
+    output = client.recv(int(output_buf)).decode()
+    print()
+    print(output)
